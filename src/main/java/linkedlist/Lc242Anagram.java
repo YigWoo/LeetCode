e2a58b642d849d04a4502d21355d2e1e5501c5a7
吴佚超 <wuyichao@lattebank.com>
2017-12-02 09:24:59 +0800

    lc242

---
+++ src/main/java/linkedlist/Lc242Anagram.java
@@ -0,0 +1,21 @@
+package linkedlist;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lc242Anagram {
+
+    private static boolean isAnagram(String s, String t) {
+        List<Integer> sList = s.chars().boxed().sorted().collect(Collectors.toList());
+        List<Integer> tList = t.chars().boxed().sorted().collect(Collectors.toList());
+
+        return sList.equals(tList);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println((isAnagram("rat", "car")));
+        System.out.println(isAnagram("", ""));
+    }
+
+}
