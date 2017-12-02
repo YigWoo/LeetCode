e9e557a5e1355a9cade5e3f716f48f889d37ce19
吴佚超 <wuyichao@lattebank.com>
2017-12-02 13:19:48 +0800

    LC 148

--- src/main/java/sort/Lc148SortList.java
+++ src/main/java/sort/Lc148SortList.java
@@ -7,6 +7,61 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Lc148SortList {
+    private static ListNode mergeSortList(ListNode head) {
+        return mergeSort(head);
+    }
+
+    private static ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode fastNode = head;
+        ListNode slowNode = head;
+        ListNode prevNode = null;
+
+        /* 计算 list 的中点，保存在 slowNode  */
+        /* prevNode 保存 slowNode 的上一个节点 */
+        while (fastNode != null && fastNode.next != null) {
+            prevNode = slowNode;
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;
+        }
+
+        prevNode.next = null;
+
+        ListNode first = mergeSort(head);
+        ListNode second = mergeSort(slowNode);
+
+        return merge(first, second);
+    }
+
+    private static ListNode merge(ListNode first, ListNode second) {
+        ListNode result = new ListNode(0); // dummy node
+        ListNode current = result;
+        while (first != null && second != null) {
+            if (first.val < second.val) {
+                current.next = first;
+                first = first.next;
+            } else {
+                current.next = second;
+                second = second.next;
+            }
+            current = current.next;
+        }
+
+        if (first != null) {
+            current.next = first;
+        }
+
+        if (second != null) {
+            current.next = second;
+        }
+
+        return result.next;
+    }
+
+    @SuppressWarnings("unused")
     private static ListNode slowSortList(ListNode head) {
         if (head == null) {
             return null;
@@ -45,7 +100,7 @@ public class Lc148SortList {
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(5, 6, 7, 2, 3);
         ListNode listNode = ListNodeUtil.createListNode(integers);
-        ListNode listNode1 = slowSortList(listNode);
+        ListNode listNode1 = mergeSortList(listNode);
         ListNodeUtil.printListNode(listNode1);
     }
 }
